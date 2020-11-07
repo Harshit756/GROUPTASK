4d50c329dc6efb8f852d2cefa0c80770669f8c1f
Harshit756 <54244765+Harshit756@users.noreply.github.com>
2020-11-07 20:14:45 +0530

    Update myinfo (1).py

--- myinfo (1).py
+++ myinfo (1).py
@@ -3,8 +3,11 @@ def MyDetails():
         os.system("tput setaf 7")
         print("""
 
-                        \t Name : ARTH GROUP TEAM 14
+                        \t Name : ARTH GROUP TEAM - 3
                         
-                        
+                          1.HARSHIT KHANDELWAL
+                          2.MANAS SRIVASTAV
+                          3.SHEFALI SHARMA 
+                          
         """)
         os.system("tput setaf 9")
